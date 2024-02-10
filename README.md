28404b972e6e2fbccaf67b144541f7a1f3426336
Kei Oikawa <hosikiti@gmail.com>
2024-02-10 18:54:56 +0900

    Update README

--- README.md
+++ README.md
@@ -6,4 +6,9 @@ A simple habit tracker app
 
 ## TODO
 
+- [ ] Task should not be displayed when the current day is not the specified day
+- [ ] Task edit and delete functionality
+- [ ] Improve task list UI
+- [ ] Check rendering performance
+- [ ] Write README
 - [ ] Add necessary comments
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,7 @@ import TaskList from '../features/tasks/components/TaskList';
 export default function IndexPage() {
     return (
         <div className="py-8 w-full flex flex-col items-center">
-            <div className="text-4xl py-8">Streakist</div>
-            <div className="flex flex-col py-8 items-center">
+            <div className="flex flex-col pb-8 items-center">
                 <span className="text-sm pb-2">Today</span>
                 <CurrentDateSelect />
             </div>
