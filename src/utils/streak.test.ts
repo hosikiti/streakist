fef43e5633ab23fd1d213fb1dd33e7a8cc9c9b65
Kei Oikawa <hosikiti@gmail.com>
2024-02-12 06:55:36 +0900

    fix: test messages

--- src/utils/streak.test.ts
+++ src/utils/streak.test.ts
@@ -38,37 +38,37 @@ describe('daily streak', () => {
         Days.Friday,
     ];
 
-    test('return 0 for empty history', () => {
+    test('returns 0 for empty history', () => {
         expect(getDailyStreak([], saturday, trackAllDays)).toBe(0);
     });
 
-    test('return 1 for 1 day streak', () => {
+    test('returns 1 for 1 day streak', () => {
         expect(getDailyStreak([friday], saturday, trackAllDays)).toBe(1);
     });
 
-    test('return 1 for 1 day streak done today', () => {
+    test('returns 1 for 1 day streak done today', () => {
         expect(getDailyStreak([saturday], saturday, trackAllDays)).toBe(1);
     });
 
-    test('return 2 for 2 days streak', () => {
+    test('returns 2 for 2 days streak', () => {
         expect(getDailyStreak([friday, saturday], sunday, trackAllDays)).toBe(
             2
         );
     });
 
-    test('return 0 for 0 days streak', () => {
+    test('returns 0 for 0 days streak', () => {
         expect(
             getDailyStreak([friday, saturday], nextMonday, trackAllDays)
         ).toBe(0);
     });
 
-    test('return 2 for 2 days streak when tracking weekends', () => {
+    test('returns 2 for 2 days streak when tracking weekends', () => {
         expect(getDailyStreak([saturday, sunday], sunday, trackWeekends)).toBe(
             2
         );
     });
 
-    test('return 3 for 3 days streak when tracking monday wednesday friday', () => {
+    test('returns 3 for 3 days streak when tracking monday wednesday friday', () => {
         expect(
             getDailyStreak(
                 [monday, wednesday, friday],
@@ -78,13 +78,13 @@ describe('daily streak', () => {
         ).toBe(3);
     });
 
-    test('return 1 for 1 day streak when tracking monday wednesday friday', () => {
+    test('returns 1 for 1 day streak when tracking monday wednesday friday', () => {
         expect(
             getDailyStreak([monday], wednesday, trackMondayWednesdayFriday)
         ).toBe(1);
     });
 
-    test('return 0 for 0 day streak when tracking monday wednesday friday', () => {
+    test('returns 0 for 0 day streak when tracking monday wednesday friday', () => {
         expect(
             getDailyStreak(
                 [tuesday, thursday],
@@ -96,51 +96,51 @@ describe('daily streak', () => {
 });
 
 describe('weekly streak', () => {
-    test('return 0 for empty history', () => {
+    test('returns 0 for empty history', () => {
         expect(getWeeklyStreak([], monday, 2)).toBe(0);
     });
 
-    test('return 2 for 2 days streak when you complete them in the same week', () => {
+    test('returns 2 for 2 days streak when you complete them in the same week', () => {
         expect(getWeeklyStreak([monday, wednesday], friday, 2)).toBe(2);
     });
 
-    test('return 2 for 2 days streak when you complete them in the same week with one done today', () => {
+    test('returns 2 for 2 days streak when you complete them in the same week with one done today', () => {
         expect(getWeeklyStreak([monday, friday], friday, 2)).toBe(2);
     });
 
-    test('return 0 for 0 days streak when you complete one in the last last week and not in the last week', () => {
+    test('returns 0 for 0 days streak when you complete one in the last last week and not in the last week', () => {
         expect(getWeeklyStreak([lastSunday], nextMonday, 1)).toBe(0);
     });
 
-    test('return 1 for 1 day streak when you complete one in the last week', () => {
+    test('returns 1 for 1 day streak when you complete one in the last week', () => {
         expect(getWeeklyStreak([lastSunday], monday, 1)).toBe(1);
     });
 
-    test('return 2 for 2 days streak when you complete one in the last week and one in this week', () => {
+    test('returns 2 for 2 days streak when you complete one in the last week and one in this week', () => {
         expect(getWeeklyStreak([lastSunday, monday], monday, 1)).toBe(2);
     });
 
-    test('streak increases when you exceed the target frequency', () => {
+    test('increases streaks when you exceed the target frequency', () => {
         expect(getWeeklyStreak([monday, wednesday, friday], sunday, 2)).toBe(3);
     });
 });
 
 describe('current week streak', () => {
-    test('return 0 for empty history', () => {
+    test('returns 0 for empty history', () => {
         expect(getCurrentWeekStreak([], monday)).toBe(0);
     });
 
-    test('return 1 for 1 day streak this week', () => {
+    test('returns 1 for 1 day streak this week', () => {
         expect(getCurrentWeekStreak([monday], monday)).toBe(1);
     });
 
-    test('return 3 for 3 days streak this week', () => {
+    test('returns 3 for 3 days streak this week', () => {
         expect(getCurrentWeekStreak([monday, tuesday, friday], saturday)).toBe(
             3
         );
     });
 
-    test('return 2 for 2 days streak this week', () => {
+    test('returns 2 for 2 days streak this week', () => {
         expect(
             getCurrentWeekStreak([lastSunday, tuesday, friday], saturday)
         ).toBe(2);
--- src/utils/uuid.test.ts
+++ src/utils/uuid.test.ts
@@ -1,7 +1,7 @@
 import { generateUUID } from './uuid';
 
 describe('uuid', () => {
-    it('should generate a unique id', () => {
+    test('should generate a unique id', () => {
         const id1 = generateUUID();
         const id2 = generateUUID();
         expect(id1).not.toEqual(id2);
