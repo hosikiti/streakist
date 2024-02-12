7235ee182bcd4ca8b9eef66fa04c26e1beec189a
Kei Oikawa <hosikiti@gmail.com>
2024-02-12 10:12:00 +0900

    fix: refactoring

--- src/features/tasks/hooks/useStreak.ts
+++ src/features/tasks/hooks/useStreak.ts
@@ -1,7 +1,22 @@
 import { useMemo } from 'react';
-import { getStreak } from '../task.utils';
-import { Task } from '../task.types';
+import { Task, TrackingType } from '../task.types';
+import { getDailyStreak, getWeeklyStreak } from '../../../utils/streak';
 
 export const useStreak = (task: Task, currentDate: string) => {
-    return useMemo(() => getStreak(task, currentDate), [task, currentDate]);
+    return useMemo(() => {
+        const history = task.completeHistory;
+        const dailyTrackingDays = task.trackingOptions.dailyTrackingDays || [];
+        const weeklyTrackingFrequency =
+            task.trackingOptions.weeklyTrackingFrequency || 0;
+
+        if (task.trackingType === TrackingType.Daily) {
+            return getDailyStreak(history, currentDate, dailyTrackingDays);
+        } else {
+            return getWeeklyStreak(
+                history,
+                currentDate,
+                weeklyTrackingFrequency
+            );
+        }
+    }, [task, currentDate]);
 };
